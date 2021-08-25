b49ccddf96ff60ab6fbf29e09d66fd7b25cfa186
emircankilinc <emircan_kilinc@hotmail.com>
2021-08-25 17:40:45 +0300

    Add Packer Constants class.
    -Constants will use with validating inputs.

---
+++ src/main/java/com/mobiquity/constant/PackerConstants.java
@@ -0,0 +1,10 @@
+package com.mobiquity.constant;
+
+public class PackerConstraints {
+
+	public static Double MAX_ALLOWABLE_PACKAGE_WEIGHT = 100.0;
+	public static Double MAX_ALLOWABLE_ITEM_WEIGHT = 100.0;
+	public static Integer MAX_ALLOWABLE_ITEM_COST = 100;
+	public static Integer MAX_ALLOWABLE_NUMBER_OF_ITEMS = 15;
+
+}
