c116806b576e554a9f694b703f46cdb6f09f14fa
emircankilinc <emircan_kilinc@hotmail.com>
2021-08-27 22:16:59 +0300

    Formatting APIException class.

--- src/main/java/com/mobiquity/exception/APIException.java
+++ src/main/java/com/mobiquity/exception/APIException.java
@@ -2,16 +2,16 @@ package com.mobiquity.exception;
 
 public class APIException extends Exception {
 
-  /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3141434677395845102L;
 
-public APIException(String message, Exception e) {
-    super(message, e);
-  }
+	public APIException(String message, Exception e) {
+		super(message, e);
+	}
 
-  public APIException(String message) {
-    super(message);
-  }
+	public APIException(String message) {
+		super(message);
+	}
 }
